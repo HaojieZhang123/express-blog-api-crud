53151d3962dfa967081ae0146c5e3f0958101d64
HaojieZhnag123 <haojiezhnag123@gmail.com>
2025-04-22 13:29:09 +0200

    [UPD] index controller function done

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -2,12 +2,38 @@ const express = require('express');
 
 // import array data
 const posts = require('../data/posts.js');
+const { error } = require('console');
 
 // controller functions
 
 // index
 const index = (req, res) => {
+    // if id is passed in the request, return the post with that id
+    const id = parseInt(req.params.id);
+    if(id){
+        const post = posts.find(element => element.id === id);
 
+        // if post is not found, return 404 error
+        if(!post){
+            res.status(404).json({
+                success: false,
+                error: '404 Not Found',
+                message: `Post with id ${id} not found`
+            });
+        }
+        else{
+            res.json({
+                success: true,
+                data: post
+            });
+        }
+    }
+    else{
+        res.json({
+            success: true,
+            data: posts
+        });
+    }
 };
 
 // show
